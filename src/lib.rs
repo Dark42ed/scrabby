793ca3d9f40319ef2eb20573019cbf4c75e87cb7
Dark42ed <D4rk2d@gmail.com>
2024-08-18 00:26:06 -0400

    cleanups

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ pub mod letter;
 pub use board::{Board, Direction, Word};
 pub use letter::Letter;
 
-use std::sync::Mutex;
-
 const DEFAULT_WORD_TEXT: &'static str = include_str!("../words.txt");
 lazy_static::lazy_static! {
     pub static ref DEFAULT_WORD_LIST: Vec<&'static str> = {
